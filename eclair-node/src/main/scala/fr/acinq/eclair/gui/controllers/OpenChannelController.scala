3b20ac86d28aa6689b680b504fddb7492744b79c
HashEngineering <hashengineeringsolutions@gmail.com>
2017-06-19 22:28:16 -0700

    Change protocol to allow for larger amounts than 0.042GRS (420K GRS)

--- eclair-node/src/main/scala/fr/acinq/eclair/gui/controllers/OpenChannelController.scala
+++ eclair-node/src/main/scala/fr/acinq/eclair/gui/controllers/OpenChannelController.scala
@@ -25,7 +25,7 @@ class OpenChannelController(val handlers: Handlers, val stage: Stage, val setup:
     *
     * https://github.com/lightningnetwork/lightning-rfc/blob/master/02-peer-protocol.md#requirements
     */
-  val maxFunding = 16777216L
+  val maxFunding = 16777216L *100000000L
   val maxPushMsat = 1000L * maxFunding
 
   @FXML var host: TextField = _
